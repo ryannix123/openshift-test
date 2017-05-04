5e61099d042014672dde893652cd90b1099efd98
Ryan T Nix <ryan.nix@gmail.com>
2017-05-03 22:25:23 -0400

    Webhook demo

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Openshift Rocks!</title>
  </head>
 <IMG SRC="http://i2.cdn.turner.com/money/dam/assets/150817122310-northwestern-football-union-780x439.jpg">
- <?php echo '<p>Openshift Demo Webhook. Prety cool, huh?' ?> 
+ <?php echo '<p>Openshift Webhook Demo. Prety cool, huh?' ?> 
  </body>
 </html>
