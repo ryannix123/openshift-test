74831b5dc3c3e64db025c111687a818374d64e27
Ryan T Nix <r-nix@northwestern.edu>
2017-05-08 16:13:59 -0500

    link update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Openshift Rocks!</title>
  </head>
 <IMG SRC="http://i2.cdn.turner.com/money/dam/assets/150817122310-northwestern-football-union-780x439.jpg">
- <?php echo '<p>Openshift Webhook Demo. Works great! Hi Marcelo!' ?> 
+ <?php echo '<p>Openshift Webhook Demo. Works great! Login today with your NetID <a href="https://nudev2.northcentralus.cloudapp.azure.com:8443/console/">here</a> to go to the portal.' ?> 
  </body>
 </html>
