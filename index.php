e331035d74314581cb7dbe4c63666b3f7c7e1ba4
Ryan T Nix <ryan.nix@gmail.com>
2017-06-01 10:16:05 -0500

    Demo

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Openshift Rocks!</title>
  </head>
 <IMG SRC="http://i2.cdn.turner.com/money/dam/assets/150817122310-northwestern-football-union-780x439.jpg">
- <?php echo '<p>Openshift Webhook Demo. Works great! Hi Tanner! Login today with your NetID <a href="https://nudev2.northcentralus.cloudapp.azure.com:8443/console/">here</a>.' ?> 
+ <?php echo '<p>Openshift Webhook Demo. Works great! Hi Guys! Login today with your NetID <a href="https://nudev2.northcentralus.cloudapp.azure.com:8443/console/">here</a>.' ?> 
  </body>
 </html>
